61e6bf98878f74dd233e8b50728d35aaec26b0cd
naveen <apnaveen108@gmail.com>
2026-09-07 09:37:16 +0530

    second

---
+++ app.py
@@ -0,0 +1,19 @@
+# Simple Python program
+
+name = input("Enter your name: ")
+
+print("Hello,", name)
+print("Welcome to Python!")
+
+# Simple calculation
+a = 10
+b = 20
+
+sum_result = a + b
+
+print("Sum:", sum_result)
+
+alladi Prema Naveen  jio
+
+
+sdsdsdhsdhsdjisdjis
